ab6307e7bac77b5028f6ba6272df6c31c1a86055
CupOfTea696 <cupoftea696@gmail.com>
2015-12-08 15:54:13 +0000

    getPage -> get

--- src/js/Application/ContentEditor.js
+++ src/js/Application/ContentEditor.js
@@ -6,16 +6,16 @@
     };
     
     var cleanUp = function () {
-        app.Page.getPage().find('[style]:not(.videoLoader):not(object)').removeAttr('style');
-        app.Page.getPage().find('[contenteditable], [aria-disabled], [data-mtifont], .ui-resizable, .onEdit')
+        app.Page.get().find('[style]:not(.videoLoader):not(object)').removeAttr('style');
+        app.Page.get().find('[contenteditable], [aria-disabled], [data-mtifont], .ui-resizable, .onEdit')
             .removeClass('onEdit ui-resizable')
             .removeAttr('contenteditable aria-disabled data-mtifont');
-    }
+    };
     
     var getCreditsHtml = function() {
         clearHtml();
         
-        app.Page.getPage().find('[class^="credits"]').clone().appendTo($html);
+        app.Page.get().find('[class^="credits"]').clone().appendTo($html);
         
         return $html.html();
     };
@@ -23,7 +23,7 @@
     var getContentHtml = function() {
         clearHtml();
         
-        app.Page.getPage()
+        app.Page.get()
             .find('.magazineContent div:not([class^="credits"]):not(.edLetterList):not(.videoHolder)').clone().appendTo($html);
         
         return $html.html();
@@ -32,7 +32,7 @@
     var getVideoHtml = function() {
         clearHtml();
         
-        app.Page.getPage().find('.videoHolder #videojs').clone().appendTo($html);
+        app.Page.get().find('.videoHolder #videojs').clone().appendTo($html);
         
         return $html.html();
     };
