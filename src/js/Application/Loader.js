65388f7d9504e02f36fcd86d46a417c862db0982
CupOfTea696 <cupoftea696@gmail.com>
2016-02-02 17:39:57 +0000

    layout

--- src/js/Application/Loader.js
+++ src/js/Application/Loader.js
@@ -1,6 +1,7 @@
 (function(window, $, app) {
     var base_uri = 'http://magtool.local/';
     var suffix = '?v=' + app.version;
+    var fa = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css';
     var css = 'css/app.css';
     var tpl = 'tpl/magtool.html';
     
@@ -11,6 +12,10 @@
         $.when(
             $.get(base_uri + css + suffix).done(function() {
                 $('<link>').attr({'rel': 'stylesheet', 'href': base_uri + css + suffix})
+                    .appendTo($('head'));
+            }),
+            $.get(fa).done(function() {
+                $('<link>').attr({'rel': 'stylesheet', 'href': fa})
                     .appendTo($('head'));
             }),
             $.get(base_uri + tpl + suffix).done(function(data) {
