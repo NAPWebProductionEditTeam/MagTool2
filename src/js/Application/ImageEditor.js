0e39048d4fea729c2d94c603a968023a60930c86
samzhang0921 <13521526089zyp@gmail.com>
2016-02-23 17:10:10 +0000

    f22

--- src/js/Application/ImageEditor.js
+++ src/js/Application/ImageEditor.js
@@ -1,19 +1,20 @@
 (function(window, $, app) {
     function ImageEditor() {
         
+        this.detectImageUrl = function() {
 
-        this.detectImageUrl =function (){
+            var imgUrl = app.ContentEditor.getSelection().find('img').attr('src');
+            app.UI.getUI().find('#imageURL').attr('src', imgUrl);
 
-            var imgUrl= app.ContentEditor.getSelection().find('img').attr('src');
-            app.UI.getUI().find('#imageURL').attr('src',imgUrl);
-            return app.UI.getUI().find('#imageURL').attr('src',imgUrl);
+            return app.UI.getUI().find('#imageURL').attr('src', imgUrl);
 
-        }
-//         this.changeUrl = function (url){
-//             var url= app.UI.getUI().find('#imageURL').val();
-//             var app.Page.getContent()
-//
-//         }
+        };
+
+        //         this.changeUrl = function (url){
+        //             var url= app.UI.getUI().find('#imageURL').val();
+        //             var app.Page.getContent()
+        //
+        //         }
     }
     
     app.modules.ImageEditor = ImageEditor;
