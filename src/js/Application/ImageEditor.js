d6755c97d7400d12639d6b7ff896ad5c3bbbf360
samzhang0921 <13521526089zyp@gmail.com>
2016-02-23 17:00:13 +0000

    ad img text

--- src/js/Application/ImageEditor.js
+++ src/js/Application/ImageEditor.js
@@ -1,6 +1,18 @@
 (function(window, $, app) {
     function ImageEditor() {
         
+
+        this.detectImageUrl =function (){
+            var url= app.UI.getUI().find('#imageURL').val();
+            var imgUrl= app.ContentEditor.getSelection().find('img').attr('src');
+            url=imgUrl;
+
+        }
+         this.changeUrl = function (url){
+             var url= app.UI.getUI().find('#imageURL').val();
+             var app.Page.getContent()
+
+         }
     }
     
     app.modules.TextEditor = ImageEditor;
