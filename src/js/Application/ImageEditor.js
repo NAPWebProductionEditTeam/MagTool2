20a5f91284a155763b1cefdbb8860cb5a8e7ff95
samzhang0921 <13521526089zyp@gmail.com>
2016-02-24 15:51:03 +0000

    TEST1

--- src/js/Application/ImageEditor.js
+++ src/js/Application/ImageEditor.js
@@ -1,20 +1,26 @@
 (function(window, $, app) {
     function ImageEditor() {
-        
-        this.detectImageUrl = function() {
+        this.detectImage = function() {
             var $selected = app.ContentEditor.getSelection();
             var $selectionControls = app.UI.getSelectionControls();
             var currentUrl = $selected.find('img').attr('src');
-            var newURL = $selectionControls.find('#imageURL').val();
-            newURL = currentUrl;
+            var currentW = $selected.find('img').attr('width');
+            var currentH = $selected.find('img').attr('height');
 
+            $selectionControls.filter('#imageURL').val(currentUrl);
+            $selectionControls.filter('#imageWidth').val(currentW);
+            $selectionControls.filter('#imageHeight').val(currentH);
         };
 
-        //         this.changeUrl = function (url){
-        //             var url= app.UI.getUI().find('#imageURL').val();
-        //             var app.Page.getContent()
-        //
-        //         }
+        this.changeUrl = function(url) {
+            var $selected = app.ContentEditor.getSelection();
+            var currentUrl = $selected.find('img').attr('src');
+
+            if (url !== currentUrl) {
+                $selected.find('img').attr('src', url);
+            }
+        };
+
     }
     
     app.modules.ImageEditor = ImageEditor;
