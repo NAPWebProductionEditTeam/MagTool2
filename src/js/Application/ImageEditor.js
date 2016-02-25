c8406f614c824649c2f29516ba3a0cba466e7226
samzhang0921 <13521526089zyp@gmail.com>
2016-02-25 12:07:00 +0000

    update naturel width and height

--- src/js/Application/ImageEditor.js
+++ src/js/Application/ImageEditor.js
@@ -3,18 +3,16 @@
         this.detectImage = function() {
             var $selected = app.ContentEditor.getSelection();
             var $selectionControls = app.UI.getSelectionControls();
-            $selected.find('img').onlod(function() {
-                var currentUrl = $selected.find('img').attr('src');
-                var currentW = $selected.find('img').attr('width');
-                var currentH = $selected.find('img').attr('height');
-                var imgw = this.naturalWidth;
-                var imgH = this.naturalHeight;
+            var currentUrl = $selected.find('img').attr('src');
+            var currentW = $selected.find('img').attr('width');
+            var currentH = $selected.find('img').attr('height');
+            var imgw = $selected.find('img').prop('naturalWidth');
+            var imgH = $selected.find('img').prop('naturalHeight');
                 $selectionControls.filter('#IMGH').val(imgH);
                 $selectionControls.filter('#IMGH').val(imgH);
                 $selectionControls.filter('#imageURL').val(currentUrl);
                 $selectionControls.filter('#imageWidth').val(currentW);
                 $selectionControls.filter('#imageHeight').val(currentH);
-            });
         };
 
         this.changeUrl = function(url) {
@@ -25,8 +23,6 @@
                 $selected.find('img').attr('src', url);
             }
         };
-
     }
-    
     app.modules.ImageEditor = ImageEditor;
 })(window, jQuery, MagTool);
