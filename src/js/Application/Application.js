3efa79b2f0d8a0d59144b181ebe089dd19b2308e
samzhang0921 <13521526089zyp@gmail.com>
2016-03-07 17:19:10 +0000

    cta detect ()

--- src/js/Application/Application.js
+++ src/js/Application/Application.js
@@ -298,6 +298,9 @@ var MagTool = MagTool || {};
             case 'image':
                 app.ImageEditor.detectImage();
                 break;
+            case 'cta':
+                app.TextEditor.detectCta();
+                break;
         }
     }, false, true);
     
