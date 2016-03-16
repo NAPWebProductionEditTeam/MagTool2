8a4e41f8d9aed4c6c33e14975fe5404b89d07c7f
CupOfTea696 <cupoftea696@gmail.com>
2016-03-16 17:38:15 +0000

    Split cleanUp into cleanUp and cleanUpForServer + normalizeEmContent

--- src/js/Application/Exporter.js
+++ src/js/Application/Exporter.js
@@ -17,8 +17,8 @@
         /**
          * Get an Array of all Nodes, including their decendant Nodes.
          *
-         * @param  NodeList  nodes
-         * @return Array
+         * @param   {NodeList}  nodes
+         * @returns {Array}
          */
         var getAllNodes = function(nodes) {
             var nodeArray = [];
@@ -61,54 +61,106 @@
             getCloneContainer().find('[style]:not(.videoLoader)').removeAttr('style');
         };
         
+        /**
+         * Ensure none of the textContent is actually in ALL CAPS. Good for SEO.
+         *
+         * @param   {Node}  node
+         */
         var normalizeCapitalisation = function(node) {
             var regex = /\b(?:[A-Z]{2,}(?:\W+[A-Z]+)*|[A-Z]+(?:\W+[A-Z]{2,})+)\b/g; // Regex to capture ALL CAPS substrings.
             var parentNode = node.parentNode;
             
-            // If there is a parentNode and the parentNode is an Element Node.
-            if (parentNode && parentNode.nodeType == Node.ELEMENT_NODE) {
-                var $parent = $(parentNode);
-                var $uppercaseElement = $parent.filter(function() {
-                    return $(this).css('text-transform') === 'uppercase';
-                });
+            var $parent = $(parentNode);
+            var $uppercaseElement = $parent.filter(function() {
+                return $(this).css('text-transform') === 'uppercase';
+            });
+            
+            // If the parentNode has text-transform: uppercase, normalize the textContent's capitalisation to ucwords.
+            if ($uppercaseElement.length) {
+                node.textContent = node.textContent.toLowerCase().ucwords();
+            } else {
+                var text = node.textContent;
+                var matches = text.match(regex);
                 
-                // If the parentNode has text-transform: uppercase, normalize the textContent's capitalisation to ucwords.
-                if ($uppercaseElement.length) {
-                    node.textContent = node.textContent.toLowerCase().ucwords();
-                } else {
-                    var text = node.textContent;
-                    var matches = text.match(regex);
+                // If we have ALL CAPS substrings
+                if (matches) {
+                    // normalize the capitalisation to ucwords and wrap in an .upperCase span
+                    text = text.replace(regex, function(match) {
+                        return '<span class="upperCase">' + match.toLowerCase().ucwords() + '</span>';
+                    });
                     
-                    // If we have ALL CAPS substrings
-                    if (matches) {
-                        // normalize the capitalisation to ucwords and wrap in an .upperCase span
-                        text = text.replace(regex, function(match) {
-                            return '<span class="upperCase">' + match.toLowerCase().ucwords() + '</span>';
-                        });
-                        
-                        // Turn the resulting string back into an Array of Nodes.
-                        var newNodes = Array.from($.parseHTML(text));
-                        
-                        // Grab the last Node of the Array.
-                        var replaceNode = newNodes.pop();
-                        
-                        // Replace the original node with the last Node from the array.
-                        parentNode.replaceChild(replaceNode, node);
-                        
-                        // Insert all other Nodes before the replaced Node. Seems to be the only way to do this through the native DOM API.
-                        for (var j = 0; j < newNodes.length; j++) {
-                            parentNode.insertBefore(newNodes[j], replaceNode);
-                        }
+                    // Turn the resulting string back into an Array of Nodes.
+                    var newNodes = Array.from($.parseHTML(text));
+                    
+                    // Grab the last Node of the Array.
+                    var replaceNode = newNodes.pop();
+                    
+                    // Replace the original node with the last Node from the array.
+                    parentNode.replaceChild(replaceNode, node);
+                    
+                    // Insert all other Nodes before the replaced Node. Seems to be the only way to do this through the native DOM API.
+                    for (var j = 0; j < newNodes.length; j++) {
+                        parentNode.insertBefore(newNodes[j], replaceNode);
                     }
                 }
             }
         };
         
-        var cleanUp = function($container) {
-            removeVideo();
-            trimBreakTags();
-            removeStyleAttributes();
-            
+        /**
+         * Ensure braces are always outside em tags and colon always inside em tag.
+         *
+         * @param   {jQuery}  $container
+         */
+        var normalizeEmContent = function($container) {
+            $container.find('em').each(function() {
+                var em = this;
+                var prev = em.previousSibling;
+                var next = em.nextSibling;
+                var parent = em.parentNode;
+                
+                var $em = $(em);
+                var matches;
+                
+                // If there is a nextSibling and it starts with a colon, move the colon inside the em tag.
+                if (next && next.textContent.match(/^:/)) {
+                    next.textContent = next.textContent.slice(1, next.textContent.length);
+                    $em.text($(em).text() + ':');
+                }
+                
+                matches = $(em).text().match(/^(\s*\()/);
+                
+                // If the content starts with a brace, optionally preceded by whitespace, move it to the previousSibling Node.
+                if (matches) {
+                    // If there is no previous sibling, create a new Text Node.
+                    if (! prev) {
+                        prev = new Text();
+                        
+                        parent.insertBefore(prev, em);
+                    }
+                    
+                    prev.textContent = prev.textContent + matches[1];
+                    $em.text($(em).text().replace(matches[1], ''));
+                }
+                
+                matches = $(em).text().match(/(\)\s*)$/);
+                
+                // If the content ends with a brace, optionally proceded by whitespace, move it to the nextSibling Node.
+                if (matches) {
+                    // If there is no next sibling, create a new Text Node.
+                    if (! next) {
+                        next = new Text();
+                        
+                        parent.appendChild(next);
+                    }
+                    
+                    next.textContent = matches[1] + next.textContent;
+                    $em.text($(em).text().replace(matches[1], ''));
+                }
+            });
+        };
+        
+        var cleanUp = function() {
+            var $container = app.Page.getContent();
             var nodes = getAllNodes($container.get(0).childNodes);
             
             // Loop over all Nodes and normalize Text Nodes' content.
@@ -119,6 +171,14 @@
                     normalizeCapitalisation(node);
                 }
             }
+            
+            normalizeEmContent($container);
+        };
+        
+        var cleanUpForServer = function($container) {
+            removeVideo();
+            trimBreakTags();
+            removeStyleAttributes();
         };
         
         var getHtml = function($elements) {
@@ -129,7 +189,8 @@
             
             $elements.clone().appendTo($cloneContainer);
             
-            cleanUp($cloneContainer);
+            cleanUp();
+            cleanUpForServer($cloneContainer);
             
             html = $cloneContainer.html();
             html = minify(html, {
