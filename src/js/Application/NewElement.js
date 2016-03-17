713d61944a694f82f6a6c6ce87760651dea2d5d3
CupOfTea696 <cupoftea696@gmail.com>
2016-03-17 10:56:26 +0000

    NewElement refactoring

--- src/js/Application/NewElement.js
+++ src/js/Application/NewElement.js
@@ -3,58 +3,87 @@
 (function(window, $, app) {
     function NewElement() {
         
-        // Add the new element to the DOM
-        var addToDom = function($elements) {
+        /**
+         * Add the new element to the DOM
+         *
+         * @param   {jQuery}  $el
+         */
+        var addToDom = function($el) {
             var $content = app.Page.getContent();
-
-            // If a slug exists, add the new element after the slug, else add it to the top of the page
             var $slug = app.Slug.findSlug();
-
+            
+            // If a slug exists, add the new element after the slug, else add it to the top of the page
             if ($slug.length) {
-                $slug.after($elements);
+                $slug.after($el);
             } else {
-                $elements.prependTo($content);
+                $el.prependTo($content);
             }
             
             // Deselect all other elements
             app.ContentEditor.deselectAll();
             
             // Apply edit interactions to the new element.
-            app.ContentEditor.applyInteractions($elements);
+            app.ContentEditor.applyInteractions($el);
             
             // Select the new Element
-            app.ContentEditor.select($elements);
+            app.ContentEditor.select($el);
         };
         
-        // Create New Text Element
+        /**
+         * Create a new container div.
+         *
+         * @param   {string} interactions
+         * @returns {jQuery}
+         */
+        var createContainer = function(interactions) {
+            var $container = $('<div/>');
+            
+            return $container.addClass('span-12 textAlignCenter push-down-18 push-right-18 ui-selectee ' + interactions);
+        };
+        
+        /**
+         * Create New Text Element.
+         */
         this.newText = function() {
-            var $newDiv = $('<div/>', {
-                class: 'span-12 textAlignCenter push-down-18 push-right-18 editable resizable draggable ui-selectee'
-            });
-            $newDiv.text('NEW EMPTY TEXT ELEMENT');
-            addToDom($newDiv);
+            var $container = createContainer('draggable resizable editable');
+            var $p = $('<p/>');
+            
+            $p.text('NEW EMPTY TEXT ELEMENT')
+              .appendTo($container);
+            
+            addToDom($container);
         };
         
-        // Create New Image Element
+        /**
+         * Create New Image Element.
+         */
         this.newImage = function() {
-            var $newDiv = $('<div/>', {
-                class: 'span-12 textAlignCenter push-down-18 push-right-18 resizable draggable ui-selectee'
-            });
-
-            $newDiv.append('<img src="http://lorempixel.com/image_output/cats-q-c-200-200-9.jpg" alt="net-a-porter" data-img-src@2x="http://lorempixel.com/image_output/cats-q-c-200-200-9.jpg" />');
-            console.log("img = " + $newDiv);
-            addToDom($newDiv);
+            var $container = createContainer('resizable draggable');
+            var $img = $('<img/>');
+            
+            $img.attr('src', 'http://lorempixel.com/image_output/cats-q-c-200-200-9.jpg')
+                .attr('data-img-src-2x', 'http://lorempixel.com/image_output/cats-q-c-400-400-9.jpg')
+                .attr('alt', 'net-a-porter')
+                .appendTo($container);
+            
+            addToDom($container);
         };
         
-        // Create New CTA Element
+        /**
+         * Create New CTA Element.
+         */
         this.newCTA = function() {
-            var $newDiv = $('<div/>', {
-                class: 'btnShopThe span-12 textAlignCenter push-down-18 push-right-18 editable resizable draggable ui-selectee'
-            });
-
-            // --> create new element and then .appenTo($newDiv)
-            $newDiv.append('<a data-magtool="ntk" href="${CtaLinkXML[\'ntk\'].@url}">SHOP THE SELECTION</a>');
-            addToDom($newDiv);
+            var $container = createContainer('editable resizable draggable');
+            var $a = $('<a/>');
+            
+            $container.addClass('btnShopThe');
+            
+            $a.attr('href', '')
+              .attr('data-magtool', 'ntk')
+              .text('SHOP THE SELECTION')
+              .appendTo($container);
+            
+            addToDom($container);
         };
     }
     
