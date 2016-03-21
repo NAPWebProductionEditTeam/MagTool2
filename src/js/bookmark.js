f1e5201a28d7e6f9941f987756d209169a547d1c
CupOfTea696 <cupoftea696@gmail.com>
2016-03-21 12:23:52 +0000

    Improve env method

--- src/js/bookmark.js
+++ src/js/bookmark.js
@@ -15,33 +15,37 @@ var MagTool = MagTool || {};
             return ENV;
         }
         
-        switch (ENV) {
-            case 'dev':
-            case 'local':
-            case 'development':
-                return env === 'development';
-            case 'test':
-            case 'testing':
-                return env === 'testing';
-            case 'dist':
-            case 'production':
-            case 'distribution':
-                return env === 'production';
+        var dev = ['dev', 'local', 'development'];
+        var test = ['test', 'staging', 'testing'];
+        var dist = ['dist', 'live', 'production', 'distribution'];
+        
+        if ($.inArray(ENV, dev) > -1) {
+            return $.inArray(env, dev) > -1;
         }
+        
+        if ($.inArray(ENV, test)) {
+            return $.inArray(env, test) > -1;
+        }
+        
+        if ($.inArray(ENV, dist)) {
+            return $.inArray(env, dist) > -1;
+        }
+        
+        return false;
     };
     
     // Needs to change to Alfresco once first dist is uploaded.
     app.BASE_URI = 'http://magtool.local/build/';
     
-    if (app.env('production')) {
+    if (app.env('dist')) {
         app.BASE_URI = 'http://magtool.local/build/';
     }
     
     app.getVersion = function(callback) {
-        var version_url = app.env('development') ? git_commit : git_release;
+        var version_url = app.env('dev') ? git_commit : git_release;
         
         return $.get(version_url).done(function(data) {
-            app.VERSION = app.env('development') ? data.object.sha : (data.length === 0 ? '0.0.0' : data[0].tag_name.replace('v', ''));
+            app.VERSION = app.env('dev') ? data.object.sha : (data.length === 0 ? '0.0.0' : data[0].tag_name.replace('v', ''));
             
             if (typeof callback === 'function') {
                 callback();
