a668d5bd9bc19f2f73c0ab59b502b4e8308a42aa
CupOfTea696 <cupoftea696@gmail.com>
2016-04-05 14:08:24 +0100

    post-commit does full build for you

--- post-commit.sh
+++ post-commit.sh
@@ -24,11 +24,11 @@ if [ `git rev-parse --abbrev-ref HEAD` == "master" ]; then
 	# Layout prefix is prepended to each markdown file synced
 	###################################################################
 	LAYOUT_PREFIX='---\nlayout: default\n---\n'
-
+    
 	# Switch to build_site branch to sync it with master
 	###################################################################
 	git checkout build_site
-
+    
 	# Sync the README.md in master to index.md adding jekyll header
 	###################################################################
 	git checkout master -- README.md
@@ -56,7 +56,31 @@ if [ `git rev-parse --abbrev-ref HEAD` == "master" ]; then
         mv temp "${file/\.//index.}"
         rm "$file"
 	done
-
+    
 	git add docs
 	git commit -a -m "Sync README and docs from master branch to build_site"
+    
+    # Build the site
+    echo "build start"
+    /usr/local/bin/jekyll build
+    echo "build done"
+    
+    git add *
+	git commit -a -m "Built site on `date +'%Y-%m-%d %H:%M:%S'`"
+    
+    # Push build_site
+	git push origin build_site
+    
+    # Switch to gh-pages branch to sync it with build_site /_site
+    git checkout gh-pages
+    
+    git checkout build_site -- _site
+    cp -r _site/ .
+    rm -rf _site
+    git add *
+    git commit -a -m "Site updated on `date +'%Y-%m-%d %H:%M:%S'`"
+    git push origin gh-pages
+    
+    # Finally, switch back to the master branch and exit block
+    git checkout master
 fi
